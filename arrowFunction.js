e5331bc203533b7aedeafedf98fe5815daf5bbc9
Victor Li <victorisnice@Victors-Air.home>
2022-01-05 09:37:03 -0500

    arrowFunction

---
+++ arrowFunction.js
@@ -0,0 +1,20 @@
+//Example 1 
+//** */ //Normal function returning sum
+ function sum(x, y) { return x + y; } // Arrow Function equivalent
+ // To call an arrow function and reuse it, you need to store it in a variable, for example: 
+ let sum2 = (x, y) => x + y; 
+
+ console.log(sum(100,1))
+ console.log(sum2(2,3))
+
+ //=========================================================== 
+ //Example 2 
+ //** */ //Normal function returning if a number is negative 
+ function isNegative(num) { return num < 0; } 
+ // Arrow Function equivalent
+ // To call an arrow function and reuse it, you need to store it in a variable, for example: 
+  let isNegative2 = (num) => num < 0; // // More info on arrow function vs normal function //
+
+  console.log(isNegative(-1))
+  console.log(isNegative2(1))
+  https://blog.bitsrc.io/arrow-functions-vs-regular-functions-in-javascript-458ccd863bc1
